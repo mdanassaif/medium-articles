7b46b9909da5f1acf059e22a57568b856fd60779
Mohd Khan <oliwertwist9@gmail.com>
2024-06-07 10:06:11 +0530

    eslintignoredbuild

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['cdn-images-1.medium.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+      },
   };
   
   export default nextConfig;
